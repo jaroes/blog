6f8edbe07ae994e7e3995c5fc225333221b5f689
Jarod Escobar <escobar.jarod@gmail.com>
2021-01-31 09:16:03 -0600

    buaha

--- db.py
+++ db.py
@@ -30,7 +30,7 @@ def init_db():
 @with_appcontext
 def init_db_command():
     init_db()
-    click.echo('DB setted')
+    click.echo('DB setted. Data deleted')
 
 def init_app(app):
     app.teardown_appcontext(close_db)
