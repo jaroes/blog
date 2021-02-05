f6fa9f785f48bb5ae1bb0a731167417d6b003601
Jarod Escobar <escobar.jarod@gmail.com>
2021-02-04 20:10:19 -0600

    profile remake

--- blog.py
+++ blog.py
@@ -8,7 +8,10 @@ from werkzeug.exceptions import abort
 from blogpage import auth
 from blogpage.auth import login, login_required
 from blogpage.db import get_db
-from blogpage.timeline import getpost, getcom, getpag
+from blogpage.timeline import (
+    getpost_profile, getpost_all, getpost_one,
+    getpag_i, getpag
+)
 
 from datetime import datetime
 
@@ -34,114 +37,79 @@ def index(pag = 0):
         except:
             way = 'asc'
 
-    posts = getpost(limit_t, limit_d, way)
+    posts = getpost_all(limit_t, limit_d, way)
 
     if way == 'asc':
         posts.reverse()
-    
+
+    print('hasta aqui sin problemas')
+
     return render_template(
         'blog/index.html', 
         posts=posts, 
         name=g.user['username'],
-        navi=getpag('global', pag),
+        navi=getpag_i('global', pag),
         pag=pag,
         num=len(posts)
     )
     
 
 
+@bp.route('/profile')
+@bp.route('/profile/<usr>', methods=['GET', 'POST'])
 @bp.route('/profile/<usr>/<int:pag>', methods=['GET', 'POST'])
-def profile(usr, pag):
-    author = True
-    sig = None
-    db, cursor = get_db()
-
-    owner = {
-        'id': g.user['id'],
-        'username': g.user['username']
-    }
-    if owner['username'] != usr:
-        cursor.execute(
-            '''
-            select id, username, entries from user where username = %s
-            ''', (usr, )
-        )
-        owner = cursor.fetchone()
-        if owner is None:
-            return redirect(url_for('blog.index'))
-        author = None
-
+def profile(usr = None, pag = None):
+    
+    #in case an unlogged enter to profile
+    if usr is None and pag is None:
+        if g.user == None:
+            return redirect(url_for('auth.login'))
+        else:
+            usr = g.user['username']
+            pag = 0
+    
+    limit_d = '1999-01-01 00:00:00'
+    limit_t = '2999-01-01 00:00:00'
+    way = 'desc'
     if request.method == 'POST':
-        next = request.form['next']
-        cursor.execute (
-            'select * from post where created_by = %s and id < %s order by id desc limit 5', (owner['id'], next)
-        )
-    else:
-        cursor.execute (
-            '''
-            select p.id, p.title, p.content, p.created_by, \
-            p.last_modified from post p where \
-            p.created_by = %s order by p.id desc limit 5
-            ''', (owner['id'], )
-        )
-    
-    posts = cursor.fetchall()
+        try:
+            if request.form['limit_d'] is not None:
+                limit_d = request.form['limit_d']
+        except:
+            way = 'desc'
 
+        try:
+            if request.form['limit_t'] is not None:
+                limit_t = request.form['limit_t']
+        except:
+            way = 'asc'
     
+    posts = getpost_profile(usr, limit_d, limit_t, way)
+
+    db, cursor = get_db()
     cursor.execute(
         '''
-        select p.birthday, p.bio, \
-        p.direction, p.pfp, p.anniversary, p.entries \
-        from profile p where p.id = %s
-        ''', (owner['id'], )
+        select p.id, p.birthday, p.bio, \
+        p.direction, p.pfp, p.anniversary, p.entries, \
+        u.username from profile p join user u where \
+        p.id = u.id and u.username = %s
+        ''', (usr, )
     )
     user_info = cursor.fetchone()
-    if user_info['entries'] > pag + 1:
-        sig = True
-        
 
+    if way == 'asc':
+        posts.reverse()
+
+    navi = getpag('profile', user_info['id'], pag)
     return render_template(
         'blog/profile.html',
         posts=posts,
         pf=user_info,
-        name=owner['username'],
-        au=author,
-        usern=g.user['username'],
-        si = sig,
-        nimpi = len(posts),
-        yupi = pag + 1
-    )
-
-
-
-@bp.route('/profile')
-@login_required
-def profile_user():
-    pages = False
-    author = None
-    db, cursor = get_db()
-
-    cursor.execute (
-        '''
-        select p.id, p.title, p.content, p.created_by, \
-        p.last_modified from post p where \
-        created_by = %s order by p.last_modified desc limit 5
-        ''', (g.user['id'], )
-    )
-    posts = cursor.fetchall()
-
-    
-    cursor.execute(
-        '''
-        select p.birthday, p.bio, \
-        p.direction, p.pfp, p.anniversary \
-        from profile p where p.id = %s
-        ''', (g.user['id'], )
-    )
-    user_info = cursor.fetchone()
-    return render_template(
-        'blog/profile.html',posts=posts,
-        pf=user_info,name=g.user['username'], au=True
+        name=g.user['username'],
+        navi=navi,
+        pag=pag,
+        num=len(posts),
+        edition=(g.user['id'] == user_info['id'])
     )
 
 
@@ -189,18 +157,12 @@ def edit(post_id):
         db.commit()
         return redirect(url_for('blog.index'))
     
-    cursor.execute(
-        '''
-        select p.id, p.title, p.content, p.created_by \
-        from post p where p.created_by = %s and p.id = %s
-        ''', (g.user['id'], post_id)
-    )
-    pst = cursor.fetchone()
-    if pst is None:
+    pst = getpost_one(post_id)
+    if pst is None or pst['username'] != g.user['username']:
         flash('No puedes editar post que no sea tuyos!!')
         return redirect(url_for('blog.index'))
     
-    return render_template('blog/edit.html', posts=pst, name=g.user['id'])
+    return render_template('blog/edit.html', posts=pst, name=g.user['username'])
 
 
 @bp.route('/delete/<int:post_id>', methods=['GET', 'POST'])
@@ -223,15 +185,10 @@ def delete(post_id):
         )
         db.commit()
         return redirect(url_for('blog.index'))
-    cursor.execute(
-        '''
-        select p.id, p.title, p.content, p.created_by \
-        from post p where p.created_by = %s and p.id = %s
-        ''', (g.user['id'], post_id)
-    )
-    posts = cursor.fetchone()
-    if posts is None:
-        flash('No puedes eliminar el post de alguien más')
+    
+    posts = getpost_one(post_id)
+    if posts is None or posts['username'] != g.user['username']:
+        flash('No puedes borrar post que no sea tuyos!!')
         return redirect(url_for('blog.index'))
     
     return render_template('blog/delete.html', post=posts)
--- timeline.py
+++ timeline.py
@@ -9,7 +9,7 @@ paged_by_set = {
 pestructure = {
     'base': 'select p.id, p.title, p.content, p.last_modified, u.username from post p inner join user u ',
     'one': 'where p.created_by = u.username and p.id = %s',
-    'of': 'where p.created_by = u.id and u.id = %s and p.last_modified > %s and p.last_modified < %s order by p.last_modified ',
+    'of': 'on p.created_by = u.id and u.username = %s and p.last_modified > %s and p.last_modified < %s order by p.last_modified ',
     'all': 'on p.created_by = u.id and p.last_modified > %s and p.last_modified < %s order by p.last_modified ',
     'desc': 'desc limit 10',
     'asc': 'asc limit 10'
@@ -22,26 +22,26 @@ cestructure = {
     'all': 'where c.created_by = u.id and c.comment_to = %s and c.created_at > %s and c.created_at < %s order by c.created_at limit 10'  
 }
     
-def getpost(id):
+def getpost_one(id):
     db, c = get_db()
     c.execute(
         '''
         select p.id, p.title, p.content, p.last_modified, \
         u.username from post p inner join user u \
-        where p.created_by = u.username and p.id = %s
+        where p.created_by = u.id and p.id = %s
         ''', (id, )
     )
     return c.fetchone()
 
-def getpost(profile_id, limit_d, limit_t, way):
+def getpost_profile(profile_name, limit_d, limit_t, way):
     db, c = get_db()
     c.execute(
         pestructure['base'] + pestructure['of'] + pestructure[way],
-        (profile_id, limit_d, limit_t)
+        (profile_name, limit_d, limit_t)
     )
     return c.fetchall()
 
-def getpost(limit_t, limit_d, way):
+def getpost_all(limit_t, limit_d, way):
     db, c = get_db()
     c.execute(
         pestructure['base'] + pestructure['all'] + pestructure[way],
@@ -74,9 +74,26 @@ def getcom(post_id, limit_t, limit_d):
 
 
 
-def getpag(fr, pag):
+def getpag_i(fr, pag):
     db, c = get_db()
     c.execute (paged_by_set[fr])
+    next = c.fetchone()
+    navi = {
+        'next': None,
+        'back': None
+    }
+    if next is not None:
+        if next['entries'] > pag:
+            navi['next'] = True
+        if pag > 0:
+            navi['back'] = True
+    
+    return navi
+
+
+def getpag(fr, id, pag):
+    db, c = get_db()
+    c.execute (paged_by_set[fr], (id, ))
     next = c.fetchone()
     navi = {
         'next': None,
